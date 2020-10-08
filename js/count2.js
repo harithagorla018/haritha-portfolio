d7aad984ce7dbb3806756bf9e1e74e21a1df4c15
harithagorla018 <72003178+harithagorla018@users.noreply.github.com>
2020-10-08 21:55:40 +0530

    portfolio

---
+++ js/count2.js
@@ -0,0 +1,9 @@
+var cu = new counterUp({
+	start: 0,	
+	duration: 6000,
+	intvalues: true,
+	interval: 100,
+	// prepend: '€',
+	// append: '.00'
+});
+cu.start();
---
+++ js/sample.js
@@ -0,0 +1,4 @@
+function myHome {
+	document.getElementById("demo").style.display = "block";
+	document.getElementById("particles").style.display="none";
+}
